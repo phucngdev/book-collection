f486a75add9bb1623880a6c86d1c9d72d4c226e9
phucngdev <98637507+phucngdev@users.noreply.github.com>
2025-01-12 15:51:26 +0700

    🍀2nd update
    title, favicon
    filter by category

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,15 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 import Books from "./pages/Books";
 import { Route, Routes } from "react-router-dom";
+import Detail from "./pages/Detail";
 
 function App() {
   return (
     <>
-      {/* <Suspense fallback={<Loading />}> */}
       <Routes>
         <Route path="/" element={<Books />} />
+        <Route path="/:id" element={<Detail />} />
       </Routes>
-      {/* </Suspense> */}
     </>
   );
 }
--- src/pages/Books.jsx
+++ src/pages/Books.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { formatDate, formatPrice } from "../utils/format";
-import { SearchOutlined } from "@ant-design/icons";
+import { CoffeeOutlined, SearchOutlined } from "@ant-design/icons";
 import { Input, Modal } from "antd";
 import categories from "../data/categories";
 import animes from "../data/anime";
@@ -17,14 +17,50 @@ const getGreeting = () => {
 };
 
 const Books = () => {
+  const [books, setBooks] = useState(() => {
+    return animes;
+  });
+
   const [search, setSearch] = useState("");
+  const [listSearch, setListSearch] = useState([]);
+  const [category, setCategory] = useState({
+    id: 0,
+    name: "All books",
+  });
   const [isModalOpen, setIsModalOpen] = useState(false);
+
   const showModal = () => {
     setIsModalOpen(true);
   };
   const handleCancel = () => {
     setIsModalOpen(false);
+    setSearch("");
+    setListSearch([]);
   };
+
+  const handleSearch = () => {
+    const filteredBooks = animes.filter((book) =>
+      book.title.toLowerCase().includes(search.toLowerCase())
+    );
+    setListSearch(filteredBooks);
+  };
+
+  useEffect(() => {
+    handleSearch();
+  }, [search]);
+
+  useEffect(() => {
+    if (!category) {
+      setBooks(animes);
+      return;
+    }
+    const { name, id } = category;
+    const filteredBooks =
+      name === "All books" || id === 0
+        ? animes
+        : animes.filter((book) => book.genre.includes(name));
+    setBooks(filteredBooks);
+  }, [category, animes]);
 
   return (
     <>
@@ -41,6 +77,22 @@ const Books = () => {
           onChange={(e) => setSearch(e.target.value)}
           className="mb-3"
         />
+        {search &&
+          listSearch &&
+          listSearch.map((book) => (
+            <div
+              className="hover:shadow-lg hover:bg-slate-200 bg-white p-4 flex items-center gap-3 cursor-pointer"
+              key={book.id}
+              onClick={() => {
+                // navigate(`/chi-tiet/${book.title}`);
+                setIsModalOpen(false);
+                setSearch("");
+              }}
+            >
+              <img src={book.imageUrl} alt={book.title} className="w-10 " />
+              {book.title}
+            </div>
+          ))}
       </Modal>
       <div className="p-2">
         <div className="flex items-center justify-between">
@@ -64,65 +116,93 @@ const Books = () => {
               </p>
             </div>
             <div className="text-2xl text-white font-jetbrains font-bold">
-              10
+              {animes.length}
             </div>
           </div>
           <div className="h-full flex items-center justify-center p-5 bg-white rounded-lg">
             <img
               src="https://product.hstatic.net/200000343865/product/6_952c89aa38ea46d48cc79e1cacc2edb0_compact.jpg"
-              className="rounded-lg object-cover w-full h-full"
+              className="rounded-lg object-cover h-full"
               alt=""
             />
           </div>
         </div>
         <div className="flex items-center gap-2 overflow-x-auto mt-5">
-          <div className="flex items-center justify-center px-3 py-1 bg-black bg-opacity-5 rounded-full cursor-pointer">
-            <span className="text-sm text-black whitespace-nowrap">
-              All books
-            </span>
+          <div
+            onClick={() =>
+              setCategory({
+                id: 0,
+                name: "All books",
+              })
+            }
+            className={`flex items-center justify-center px-3 py-1 rounded-full cursor-pointer ${
+              category.id === 0
+                ? "bg-[#416b49] text-white"
+                : "bg-black bg-opacity-5"
+            }`}
+          >
+            <span className="text-sm whitespace-nowrap">All books</span>
           </div>
           {categories.map((cate) => (
             <div
               key={cate.id}
-              className="flex items-center justify-center px-3 py-1 bg-black bg-opacity-5 rounded-full cursor-pointer"
+              onClick={() => setCategory(cate)}
+              className={`flex items-center justify-center px-3 py-1 rounded-full cursor-pointer ${
+                category.id === cate.id
+                  ? "bg-[#416b49] text-white"
+                  : "bg-black bg-opacity-5"
+              }`}
             >
-              <span className="text-sm text-black whitespace-nowrap">
-                {cate.name}
-              </span>
+              <span className="text-sm whitespace-nowrap">{cate.name}</span>
             </div>
           ))}
         </div>
         <div className="mt-5">
           <h2 className="relative text-xl font-jetbrains font-bold text-[#416b49] ps-[18px] before:content-[''] before:absolute before:left-0 before:top-0 before:h-full before:w-[5px] before:bg-[#416b49]">
-            All books
+            {category.name}
           </h2>
-          <div className="grid grid-cols-2 gap-4 my-5">
-            {animes.map((anime) => (
-              <div
-                key={anime.id}
-                className="p-2 flex flex-col rounded-lg border border-[#c7c7c7]"
-              >
-                <div className="p-3">
-                  <img
-                    src={anime.imageUrl}
-                    alt={anime.title}
-                    className="object-cover w-full h-full"
-                  />
-                </div>
-                <div className="flex-1 flex flex-col justify-between">
-                  <h4 className="text-sm">{anime.title}</h4>
-                  <div className="flex items-center justify-between mt-2">
-                    <span className="text-lg text-red-600">
-                      {formatPrice(anime.price)}
-                    </span>
-                    <span className="text-sm text-gray-400">
-                      {anime.publisher}
-                    </span>
+          {books.length > 0 ? (
+            <>
+              <div className="grid grid-cols-2 gap-4 my-5">
+                {books.map((anime) => (
+                  <div
+                    key={anime.id}
+                    className="p-2 flex flex-col rounded-lg border border-[#c7c7c7]"
+                  >
+                    <div className="p-3">
+                      <img
+                        src={anime.imageUrl}
+                        alt={anime.title}
+                        className="object-cover w-full h-full"
+                      />
+                    </div>
+                    <div className="flex-1 flex flex-col justify-between">
+                      <h4 className="text-sm">{anime.title}</h4>
+                      <div className="flex items-center justify-between mt-2">
+                        <span className="text-lg text-red-600">
+                          {formatPrice(anime.price)}
+                        </span>
+                        <span className="text-sm text-gray-400">
+                          {anime.publisher}
+                        </span>
+                      </div>
+                    </div>
                   </div>
+                ))}
+              </div>
+            </>
+          ) : (
+            <>
+              <div className="flex items-center justify-center my-10">
+                <div className="flex flex-col items-center gap-5">
+                  <CoffeeOutlined className="text-4xl text-[#416b49]" />
+                  <p className="text-sm font-jetbrains">
+                    You don't have this type of book!
+                  </p>
                 </div>
               </div>
-            ))}
-          </div>
+            </>
+          )}
         </div>
       </div>
       <div className="p-2 pt-5 border-t border-[#c7c7c7]">
